836a1fb30ae3456f7d62cb3b016f08185a4cdaf9
lodiwifidev <74971398+lodiwifidev@users.noreply.github.com>
2021-01-16 08:33:36 +0800

    cleanup

--- newhotspot.lodiwifi.net/root/lodisync_stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo Stopping directive processign script
-kill -15 `ps | grep directive | grep sh | cut -d' ' -f 2`
--- newhotspot.lodiwifi.net/root/make_directive_filename.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-MAC=`ip link show eth0 | awk -e '/^\s*link\//{print $2}' | tr ':' '-'`
-HOSTNAME=`cat /proc/sys/kernel/hostname`
-
-echo $HOSTNAME.$MAC
